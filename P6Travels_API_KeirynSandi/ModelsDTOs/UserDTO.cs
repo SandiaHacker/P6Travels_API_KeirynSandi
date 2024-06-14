e90f80f489bb4ee15555f91e5927088ea784ec57
Keiryn Sandí <keyrin@hotmail.es>
2024-06-13 23:44:48 -0600

    Semana 5

---
+++ P6Travels_API_KeirynSandi/ModelsDTOs/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace P6Travels_API_KeirynSandi.ModelsDTOs
+{
+    //Un DTO (Data Tranfer Object) es un tipo 
+    //de versión "recortada" de un modelo particular
+    //donde se pretende quitar todas las complejidades
+    //generadas en los modelos a partir del ORM (Entity
+    //Framework en nuestro caso).
+
+    //El propisito tambien puede ser ocultar la estructura
+    //original de los modelos al equipo de desarollo 
+    //Front End ya sea porque no es necesaria visualizar
+    //el modelo completo o porque no se debe mostrar la
+    //estructura original de las tablas a nivel de
+    //bases de datos.
+
+
+    public class UserDTO
+    {
+
+
+    }
+}
